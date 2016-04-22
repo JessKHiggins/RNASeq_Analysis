dcfe0770c035f35b9624ff776301618e73726ac1
JessKHiggins <jessicakhiggins@gmail.com>
2016-04-22 15:05:50 -0400

    Added plots of mean transcript levels for each condition in heat shock, immune, and trypsin like genes.

--- M_sexta_gene_name_Script.R
+++ M_sexta_gene_name_Script.R
@@ -4,6 +4,8 @@ setwd("~/Google Drive/Kingsolver Lab- Post Doc/M_sexta_RNASeq_Analysis/data")
 
 library(ggplot2)
 library(dplyr)
+library(tidyr)
+library(reshape2)
 
 total_data<- read.csv("Msex_CLC_diff.csv", header=TRUE)
 names(total_data)
@@ -32,7 +34,7 @@ names(joined)
 #Getting top 50 upregulated for each comparison of interest
 CNS_CHS_diff<- joined %>% 
   filter(EDGE_CNSvCHS_pvalue< 0.05) %>% 
-  select(Seq_uname,Seq_Description,EDGE_CNSvCHS_pvalue,EDGE_CNSvCHS_Foldchange,EDGE_CNSvCHS_Weighted_diff) %>% 
+  select(Seq_uname,Seq_Description,EDGE_CNSvCHS_pvalue,EDGE_CNSvCHS_Foldchange,EDGE_CNSvCHS_Weighted_diff, CNS_Means,CHS_Means,FNS_Means,FHS_Means) %>% 
   arrange(desc(EDGE_CNSvCHS_Foldchange))
 
 head(CNS_CHS_diff)
@@ -41,7 +43,7 @@ write.csv(top_n(CNS_CHS_diff,50, EDGE_CNSvCHS_Foldchange),"Top_50_CNSvCHS_up.csv
 
 CHS_FHS_diff<- joined %>% 
   filter(EDGE_CHSvFHS_pvalue< 0.05) %>% 
-  select(Seq_uname,Seq_Description,EDGE_CHSvFHS_pvalue,EDGE_CHSvFHS_Foldchange,EDGE_CHSvFHS_Weighted_diff) %>% 
+  select(Seq_uname,Seq_Description,EDGE_CHSvFHS_pvalue,EDGE_CHSvFHS_Foldchange,EDGE_CHSvFHS_Weighted_diff,CNS_Means,CHS_Means,FNS_Means,FHS_Means) %>% 
   arrange(desc(EDGE_CHSvFHS_Foldchange))
 
 head(CHS_FHS_diff)
@@ -50,7 +52,7 @@ write.csv(top_n(CHS_FHS_diff,50,EDGE_CHSvFHS_Foldchange),"Top_50_CHSvFHS_up.csv"
 
 FNS_FHS_diff<- joined %>% 
   filter(EDGE_FNSvFHS_pvalue< 0.05) %>% 
-  select(Seq_uname,Seq_Description,EDGE_FNSvFHS_pvalue,EDGE_FNSvFHS_Foldchange,EDGE_FNSvFHS_Weighted_diff) %>% 
+  select(Seq_uname,Seq_Description,EDGE_FNSvFHS_pvalue,EDGE_FNSvFHS_Foldchange,EDGE_FNSvFHS_Weighted_diff,CNS_Means,CHS_Means,FNS_Means,FHS_Means) %>% 
   arrange(desc(EDGE_FNSvFHS_Foldchange))
 
 head(FNS_FHS_diff)
@@ -59,7 +61,7 @@ write.csv(top_n(FNS_FHS_diff,50,EDGE_FNSvFHS_Foldchange),"Top_50_FNSvFHS_up.csv"
 
 CNS_FNS_diff<- joined %>% 
   filter(EDGE_CNSvFNS_pvalue< 0.05) %>% 
-  select(Seq_uname,Seq_Description,EDGE_CNSvFNS_pvalue,EDGE_CNSvFNS_Foldchange,EDGE_CNSvFNS_Weighted_diff) %>% 
+  select(Seq_uname,Seq_Description,EDGE_CNSvFNS_pvalue,EDGE_CNSvFNS_Foldchange,EDGE_CNSvFNS_Weighted_diff,CNS_Means,CHS_Means,FNS_Means,FHS_Means) %>% 
   arrange(desc(EDGE_CNSvFNS_Foldchange))
 
 head(CNS_FNS_diff)
@@ -68,7 +70,7 @@ write.csv(top_n(CNS_FNS_diff,50,EDGE_CNSvFNS_Foldchange),"Top_50_CNSvFNS_up.csv"
 
 CNS_FHS_diff<- joined %>% 
   filter(EDGE_CNSvFHS_pvalue< 0.05) %>% 
-  select(Seq_uname,Seq_Description,EDGE_CNSvFHS_pvalue,EDGE_CNSvFHS_Foldchange,EDGE_CNSvFHS_Weighted_diff) %>% 
+  select(Seq_uname,Seq_Description,EDGE_CNSvFHS_pvalue,EDGE_CNSvFHS_Foldchange,EDGE_CNSvFHS_Weighted_diff,CNS_Means,CHS_Means,FNS_Means,FHS_Means) %>% 
   arrange(desc(EDGE_CNSvFHS_Foldchange))
 
 head(CNS_FHS_diff)
@@ -117,9 +119,77 @@ CHS_FHS_diff %>%
 #Read in CLC_diff and look at overall levels 
 #Means are total gene reads for each condition
 
-means<- joined %>% 
-  select(Seq_uname,Seq_Description,CNS_Means,CHS_Means,FNS_Means,FHS_Means) 
+#Means that are significant in all conditions
 
-head(arrange(means,desc(CNS_Means)))
-  
+Means_join<- full_join()
+names(Means_join)
+
+top1<-top_n(CNS_CHS_diff,50, EDGE_CNSvCHS_Foldchange)
+top2<-top_n(CHS_FHS_diff,50,EDGE_CHSvFHS_Foldchange)
+top3<- top_n(CNS_FNS_diff,50,EDGE_CNSvFNS_Foldchange)
+top4<- top_n(FNS_FHS_diff,50,EDGE_FNSvFHS_Foldchange)
+
+down1<- downCNS_CHS[1:50,]
+down2<- downCHS_FHS[1:50,]
+down3<- downCNS_FNS[1:50,]
+down4<- downFNS_FHS[1:50,]
+
+join1<- full_join(top1,top2)
+join2<- full_join(top3,top4)
+means_joinup<-full_join(join1,join2)
+
+View(means_join)
+names(means_join)
+
+means_join_up<- means_joinup %>% 
+  select(Seq_uname,Seq_Description, CNS_Means, CHS_Means, FNS_Means,FHS_Means)
+
+names(means_join_up)
+
+join3<- full_join(down1,down2)
+join4<- full_join(down3,down4)
+means_joindown<-full_join(join3,join4) 
+
+means_join_down<- means_joindown %>% 
+  select(Seq_uname,Seq_Description, CNS_Means, CHS_Means, FNS_Means,FHS_Means)
+
+all_means<- full_join(means_join_up,means_join_down)
+head(all_means)
+View(all_means)
+
+
+#Filtering out tryp genes
+tryp_means<- all_means %>% 
+  filter(grepl("tryp", Seq_Description) )
+
+melt_tryp<-melt(tryp_means, id=c("Seq_uname", "Seq_Description")) %>% 
+  rename(Condition=variable, Means=value)
+
+
+#Filtering out immue genes bye using "cin"
+
+immune_means<- all_means %>% 
+  filter(grepl("cin", Seq_Description))
+
+melt_immune<-melt(immune_means, id=c("Seq_uname", "Seq_Description")) %>% 
+  rename(Condition=variable, Means=value)
+
+#Filtering out heat shock genes
+
+hs_means<- all_means %>% 
+  filter(grepl("heat", Seq_Description))
+
+melt_hs<-melt(hs_means, id=c("Seq_uname", "Seq_Description")) %>% 
+  rename(Condition=variable, Means=value)
+
+
+#Rough plots where each color is a sequence description and each point is a unique gene
+ggplot(melt_tryp, aes(x=Condition, y=Means, color=Seq_Description))+
+  geom_point()+geom_line(aes(group=Seq_uname))
+
+ggplot(melt_immune, aes(x=Condition, y=Means, color=Seq_Description))+
+  geom_point()+geom_line(aes(group=Seq_uname))
+
+ggplot(melt_hs, aes(x=Condition, y=Means, color=Seq_Description))+
+  geom_point()+geom_line(aes(group=Seq_uname))
 
